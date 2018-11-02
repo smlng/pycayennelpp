9eb3ed0e19dae4eaeb9153b49e87c93ed7a1665d
smlng <s@mlng.net>
2018-11-02 21:40:51 +0100

    style: pass flake8 checks
    Adapt existing code to adhere to PEP8 standards and pass flake8 style
    checks.

--- cayennelpp/lpp_data.py
+++ cayennelpp/lpp_data.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
-from cayennelpp.lpp_util import *
+from cayennelpp.lpp_util import (LPP_DATA_TYPE,
+                                 LppDataTypeError,
+                                 LppDataSizeError)
 
 import logging
 
@@ -8,8 +10,9 @@ import logging
 class LppData(object):
 
     def __init__(self, chn, tid, data):
-        logging.debug("LppData(channel=%d, type=%d, len=%d)", chn, tid, len(data))
-        if not tid in LPP_DATA_TYPE:
+        logging.debug("LppData(channel=%d, type=%d, len=%d)",
+                      chn, tid, len(data))
+        if tid not in LPP_DATA_TYPE:
             logging.error("LppData.from_buffer: invalid data type!")
             raise LppDataTypeError
         if not len(data) == LPP_DATA_TYPE[tid]['num']:
@@ -20,11 +23,13 @@ class LppData(object):
         self._dat = data
 
     def __str__(self):
-        return 'LppData(channel = {}, type = {}, data = {})'.format(self._chn, LPP_DATA_TYPE[self._tid]['name'], str(self._dat))
+        return 'LppData(channel = {}, type = {}, data = {})'.format(
+                self._chn, LPP_DATA_TYPE[self._tid]['name'], str(self._dat))
 
     @classmethod
     def from_bytes(class_object, bytes):
-        logging.debug("LppData.from_bytes: bytes=%s, length=%d", bytes.hex(), len(bytes))
+        logging.debug("LppData.from_bytes: bytes=%s, length=%d",
+                      bytes.hex(), len(bytes))
         if len(bytes) < 3:
             logging.error("LppData.from_bytes: invalid buffer size!")
             raise LppDataSizeError
@@ -40,15 +45,17 @@ class LppData(object):
 
     def data(self):
         logging.debug("LppData.get_data")
-        logging.debug("  out:   values = %s, num = %d", str(self._dat), len(self._dat))
+        logging.debug("  out:   values = %s, num = %d",
+                      str(self._dat), len(self._dat))
         return self._dat
 
     def bytes(self):
         logging.debug("LppData.get_bytes")
-        hdr_buf = bytearray([ self._chn, self._tid ])
+        hdr_buf = bytearray([self._chn, self._tid])
         dat_buf = LPP_DATA_TYPE[self._tid]['encode'](self._dat)
         buf = hdr_buf + dat_buf
-        logging.debug("  out:   bytes = %s, length = %d", str(buf.hex()), len(buf))
+        logging.debug("  out:   bytes = %s, length = %d",
+                      str(buf.hex()), len(buf))
         return buf
 
     def data_size(self):
@@ -59,22 +66,29 @@ class LppData(object):
         logging.debug("LppData.bytes_size")
         return (LPP_DATA_TYPE[self._tid]['size'] + 2)
 
+
 def main():
     # 01 67 FF D7 = -4.1C
-    temp_buf = bytearray([ 0x01, 0x67, 0xFF, 0xD7 ])
+    temp_buf = bytearray([0x01, 0x67, 0xFF, 0xD7])
     temp_dat = LppData.from_bytes(temp_buf)
     print("Temperature: data=%s, bytes=%s, data_size=%d, bytes_size=%d" %
-          (temp_dat.data(), temp_dat.bytes().hex(), temp_dat.data_size(), temp_dat.bytes_size()))
+          (temp_dat.data(), temp_dat.bytes().hex(),
+           temp_dat.data_size(), temp_dat.bytes_size()))
     # 06 71 04 D2 FB 2E 00 00
-    acc_buf = bytearray([ 0x06, 0x71, 0x04, 0xD2, 0xFB, 0x2E, 0x00, 0x00 ])
+    acc_buf = bytearray([0x06, 0x71, 0x04, 0xD2, 0xFB, 0x2E, 0x00, 0x00])
     acc_dat = LppData.from_bytes(acc_buf)
     print("Accelerometer: data=%s, bytes=%s, data_size=%d, bytes_size=%d" %
-          (acc_dat.data(), acc_dat.bytes().hex(), acc_dat.data_size(), acc_dat.bytes_size()))
+          (acc_dat.data(), acc_dat.bytes().hex(),
+           acc_dat.data_size(), acc_dat.bytes_size()))
     # 01 88 06 76 5f f2 96 0a 00 03 e8
-    gps_buf = bytearray([ 0x01, 0x88, 0x06, 0x76, 0x5f, 0xf2, 0x96, 0x0a, 0x00, 0x03, 0xe8 ])
+    gps_buf = bytearray([0x01, 0x88, 0x06, 0x76,
+                         0x5f, 0xf2, 0x96, 0x0a,
+                         0x00, 0x03, 0xe8])
     gps_dat = LppData.from_bytes(gps_buf)
     print("GPS: data=%s, bytes=%s, data_size=%d, bytes_size=%d" %
-          (gps_dat.data(), gps_dat.bytes().hex(), gps_dat.data_size(), gps_dat.bytes_size()))
+          (gps_dat.data(), gps_dat.bytes().hex(),
+           gps_dat.data_size(), gps_dat.bytes_size()))
+
 
 if __name__ == '__main__':
     # execute when run as program
