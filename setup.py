88cc66c24cec5dea3fa0ad06013ad9bab38f8bfc
smlng <s@mlng.net>
2018-11-02 21:15:42 +0100

    feat: add packaging files
    Provide meta data and files to allow for proper Python packaging and
    publishing on PyPi to enable installation via pip.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pycayennelpp',
+      version='0.2',
+      description='Encoder and Decoder for the Cayenne Low Power Payload (LLP)',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+      ],
+      keywords='cayenne lpp iot lora lorawan ttn',
+      url='http://github.com/smlng/pycayennelpp',
+      author='smlng',
+      author_email='s@mlng.net',
+      license='MIT',
+      packages=['cayennelpp'],
+      include_package_data=True,
+      zip_safe=False)
