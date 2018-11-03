55aaf58a882a7194e6351b11d0be82bd92149e75
smlng <s@mlng.net>
2018-11-03 21:00:10 +0100

    style: remove obsolete future imports

--- cayennelpp/lpp_data.py
+++ cayennelpp/lpp_data.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-
 from .lpp_type import LPP_DATA_TYPE
 
 import logging
--- cayennelpp/lpp_frame.py
+++ cayennelpp/lpp_frame.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-
 from .lpp_data import LppData
 
 import base64
