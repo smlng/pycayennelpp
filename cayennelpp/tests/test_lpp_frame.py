8ac06804ef47adc2307ede5ba237235787437397
smlng <s@mlng.net>
2018-11-08 22:49:02 +0100

    fix: rename bytes to buf in test_lpp_frame

--- cayennelpp/tests/test_lpp_frame.py
+++ cayennelpp/tests/test_lpp_frame.py
@@ -11,28 +11,28 @@ def frame():
 
 
 def test_frame_empty(frame):
-    assert len(frame.data) == 0
+    assert len(frame.value) == 0
     assert len(frame.bytes()) == 0
 
 
 def test_frame_from_bytes():
     # 03 67 01 10 05 67 00 FF = 27.2C + 25.5C
-    bytes = bytearray([0x03, 0x67, 0x01, 0x10, 0x05, 0x67, 0x00, 0xff])
-    frame = LppFrame.from_bytes(bytes)
-    assert bytes == frame.bytes()
-    assert len(frame.data) == 2
+    buf = bytearray([0x03, 0x67, 0x01, 0x10, 0x05, 0x67, 0x00, 0xff])
+    frame = LppFrame.from_bytes(buf)
+    assert buf == frame.bytes()
+    assert len(frame.value) == 2
 
 
 def test_frame_from_base64():
     base64 = "AYgILMMBiIMAAAACAAY="
     frame = LppFrame.from_base64(base64)
-    assert len(frame.data) == 2
+    assert len(frame.value) == 2
 
 
 def test_add_digital_io(frame):
     frame.add_digital_input(0, 21)
     frame.add_digital_output(1, 42)
-    assert len(frame.data) == 2
+    assert len(frame.value) == 2
 
 
 def test_add_analog_io(frame):
@@ -40,7 +40,7 @@ def test_add_analog_io(frame):
     frame.add_analog_input(1, -12.34)
     frame.add_analog_output(0, 56.78)
     frame.add_analog_output(1, -56.78)
-    assert len(frame.data) == 4
+    assert len(frame.value) == 4
 
 
 def test_add_sensors(frame):
@@ -48,20 +48,19 @@ def test_add_sensors(frame):
     frame.add_presence(3, 1)
     frame.add_accelerometer(5, 1.234, -1.234, 0.0)
     frame.add_pressure(6, 1005.5)
-    frame.add_barometer(9, 999.9)
     frame.add_gyrometer(7, 1.234, -1.234, 0.0)
     frame.add_gps(8, 1.234, -1.234, 0.0)
-    assert len(frame.data) == 7
+    assert len(frame.value) == 6
 
 
 def test_add_temperature(frame):
     frame.add_temperature(2, 12.3)
     frame.add_temperature(3, -32.1)
-    assert len(frame.data) == 2
+    assert len(frame.value) == 2
 
 
 def test_add_humidity(frame):
     frame.add_humitidy(2, 12.3)
     frame.add_humitidy(3, 45.6)
     frame.add_humitidy(4, 78.9)
-    assert len(frame.data) == 3
+    assert len(frame.value) == 3
