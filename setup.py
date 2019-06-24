21d10eca965b8d4920d5c4551cbfa92761f9f170
smlng <s@mlng.net>
2019-06-24 21:26:03 +0200

    chore: update to v1.2.0

--- setup.py
+++ setup.py
@@ -6,24 +6,27 @@ def readme():
         return f.read()
 
 
-setup(name='pycayennelpp',
-      version='1.1.0',
-      python_requires='>=3.4',
-      description='Encoder and Decoder for CayenneLLP',
-      long_description=readme(),
-      classifiers=[
+setup(
+    name='pycayennelpp',
+    version='1.2.0',
+    python_requires='>=3.4',
+    description='Encoder and Decoder for CayenneLLP',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.4'
-      ],
-      keywords='cayenne lpp iot lora lorawan ttn',
-      url='http://github.com/smlng/pycayennelpp',
-      author='smlng',
-      author_email='s@mlng.net',
-      license='MIT',
-      packages=['cayennelpp'],
-      setup_requires=["pytest-runner"],
-      tests_require=['pytest'],
-      include_package_data=True)
+    ],
+    keywords='cayenne lpp iot lora lorawan ttn',
+    url='http://github.com/smlng/pycayennelpp',
+    author='smlng',
+    author_email='s@mlng.net',
+    license='MIT',
+    packages=['cayennelpp'],
+    setup_requires=["pytest-runner"],
+    tests_require=['pytest'],
+    include_package_data=True
+)
