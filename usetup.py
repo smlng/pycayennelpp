74dc4fa01c86699871f5e084dee0d1014b6b9ef6
smlng <s@mlng.net>
2019-06-27 21:14:48 +0200

    chore: add usetup.py for MicroPython packaging

---
+++ usetup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import sdist_upip
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='micropython-pycayennelpp',
+    version='1.3.0',
+    description='Encoder and Decoder for CayenneLLP',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: Implementation :: MicroPython'
+    ],
+    keywords='cayenne lpp iot lora lorawan ttn',
+    url='http://github.com/smlng/pycayennelpp',
+    author='smlng',
+    author_email='s@mlng.net',
+    license='MIT',
+    packages=['cayennelpp'],
+    cmdclass={'sdist': sdist_upip.sdist},
+    install_requires=['micropython-base64'],
+    include_package_data=True
+)
