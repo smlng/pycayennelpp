83a3bf2891a50e01a7f25b7bdf600cba516e3d3e
smlng <s@mlng.net>
2020-05-03 15:47:07 +0200

    chore: update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only'
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     keywords='cayenne lpp iot lora lorawan ttn',
     url='http://github.com/smlng/pycayennelpp',
