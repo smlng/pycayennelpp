d15e6c8b1bf644bf4ddf783c053e6f08d2bcb87d
smlng <s@mlng.net>
2021-06-29 21:18:38 +0200

    docs: refined docstrings

--- cayennelpp/__init__.py
+++ cayennelpp/__init__.py
@@ -1,4 +1,8 @@
-"""A decoder and encoder for the CayenneLPP data format."""
+"""PyCayenneLPP: A decoder and encoder for the CayenneLPP data format.
+
+PyCayenneLPP offers a concise interface with proper encoding and decoding
+functionality for the CayenneLPP format, supporting many sensor types.
+"""
 
 from .lpp_data import LppData
 from .lpp_frame import LppFrame
